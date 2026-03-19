814d9c5a4e76d9232990d14c891a1d5f7f8c5385
Renee Vettivelu <renee.vettivelu08@gmail.com>
2026-03-18 22:04:06 -0400

    created projects page shell

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,15 @@
+import projects from "../data/projects";
+import ProjectCard from "../components/ProjectCard";
+
+export default function Projects() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <div className="grid">
+        {projects.map((p) => (
+          <ProjectCard key={p.id} project={p} />
+        ))}
+      </div>
+    </div>
+  );
+}
